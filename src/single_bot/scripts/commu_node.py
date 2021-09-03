2ffcbfa6357b26c5912f4d541a8c0f46872d725f
Nehal Nevle <nehalsuperdude@gmail.com>
2021-09-03 12:20:27 +0530

    communictation node added

--- src/single_bot/scripts/commu_node.py
+++ src/single_bot/scripts/commu_node.py
@@ -10,36 +10,37 @@ and send to bot.
 """
 
 import rospy
+from single_bot.msg import commu
 from single_bot.msg import com_msg
 from std_msgs.msg import String
 
-global idle_state 
+ 
 idle_state= True
 
-global com_msg0
 
 
-ctl_msg0 = com_msg(vr=0,vl=0,ifUnload=False)
+
+ctl_msg0 = commu(x=0,y=0,isUnload=False)
 
 def callback(ctl_msg,robot):
-    #write code to calculate twist message or vr,vl of robot 
-    #----
-    #----
-
-    # stop bot if bot state is switched to idle_state
+    r=0.1  #radius
+    l=1 #lenght
+    x= (r/2)*(ctl_msg.vl+ctl_msg.vr)
+    y=(r/l)*(ctl_msg.vl-ctl_msg.vr)
+    msg_robot =commu(x=x,y=y,isUnload=ctl_msg.isUnload)
     if idle_state:
-        # modify message to stop bot
+        msg_robot =commu(x=0,y=0,isUnload=ctl_msg.isUnload) 
         pass
     
     #send message to bot
-    robot.publish(ctl_msg)
+    robot.publish(msg_robot)
     # save previous control input for future developments
     ctl_msg0 = ctl_msg
     return
 
 def stop_navigation(msg):
     global idle_state
-    if msg == 'Idle': 
+    if msg.data == 'Idle': 
         idle_state = True
         rospy.loginfo('bot in idle_state')
     else:
@@ -48,7 +49,7 @@ def stop_navigation(msg):
 
 def talker():
     rospy.init_node('commu_node')
-    robot = rospy.Publisher('/robotMsgs', com_msg,queue_size=1)  # May use message format used by bot
+    robot = rospy.Publisher('/robotMsgs', commu,queue_size=1)  # May use message format used by bot
     rospy.Subscriber('/commu', com_msg,callback,robot)
     rospy.Subscriber('/state_id',String,stop_navigation)
     rospy.spin()
