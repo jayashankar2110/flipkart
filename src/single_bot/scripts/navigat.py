a9153b585054aa7da1bcc4022949341ee9aa2df7
jayashankar <jayashankar2110@gmail.com>
2021-10-03 14:58:20 +0530

    qwerty

---
+++ src/single_bot/scripts/navigat.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python3
+import rospy
+
+
+import dynamic_reconfigure.client
+from PID import PID
+import time as clock
+import pdb
+
+from single_bot.msg import localizemsg
+from single_bot.msg import com_msg
+
+import numpy as np
+
+# paramers
+look_forward = 0.20  # in cm
+   
+
+class Bot():
+    def __init__(self,id):
+        self.bot_id = id
+        self.cx=[]
+        self.cy=[] 
+        self.v = 0
+        self.w=0
+        self.c_state=None
+        self.param_client = dynamic_reconfigure.client.Client("dyn_param_server", timeout=30, config_callback=None)
+        self.debug = dict()
+        self.debug = {}
+        self.target_indx = 0
+
+    def control(self,_com_pub):
+        param_client = dynamic_reconfigure.client.Client("dyn_param_server", timeout=30, config_callback=None)
+        k = self.param_client.get_configuration(timeout=1) 
+        drive = k['start_navigation']
+        while drive and self.target_indx < len(self.cx) and not rospy.is_shutdown():
+            #GUI Control
+            drive = k['start_navigation']
+            # determine target point
+            dist = self.calc_distance(self.cx[target_indx], self.cy[target_indx])
+            if dist < look_forward:
+                self.target_indx += 1
+            tx = self.cx[target_indx]
+            ty = self.cy[target_indx]
+            #get feedback and transform error
+            theta = c_state[2]
+            x = self.c_state[0]
+            y = self.c_state[1]
+
+            T = [[math.cos(theta),math.sin(theta)],[-math.sin(theta),math.cos(theta)]]
+            Xe = np.dot(T,[[tx-x],[ty-y]]) # error vector
+            eTheta = math.atan2(Xe[1], Xe[0])
+            eTheta = -max(min(eTheta, math.pi/1.5), -math.pi/1.5)
+            ePos = np.sqrt((tx-x)^2 + (ty-y)^2)
+
+            #calculate control input
+            kv= float(k['vel_p'])
+            kp= float(k['yaw_p'])
+            p_pid.setKp(kv)
+            v = vel(i) + kv*ePos
+            w = tan(atan(kp*eTheta))
+            self.v = v
+            self.w = w
+            _com_pub.publish(v = self.v, w = self.w,ifUnload = False)
+            # break if unstable
+            if ePos > 40:
+                rospy.logwarn("bot is too far from target")
+                break
+
+            # debug output
+            self.debug['Xe'] = Xe
+            self.debug['alpha_e'] = alpha_e
+            self.debug['control'] = [v,w]
+            self.debug['target_indx'] = target_indx
+            print(self.debug)
+        print('Robot stopped')
+        self.v= 0
+        self.w= 0
+        self._param_client.update_configuration({"start_navigation":False})
+        
+
+    def calc_distance(self, point_x, point_y):
+        #pdb.set_trace()
+        x = self.c_state[0]
+        y = self.c_state[1]
+        dx = x - point_x
+        dy = y - point_y
+        return math.hypot(dx, dy)
+
+def feed_cb(msg,bot):
+    #if bot.bot_id in msg.id:
+    bot.c_state = [msg.x_cordinate,msg.y_cordinate,msg.angle,msg.velocity] 
+        #yaw = self.wrap2PI(float(msg.angle))
+
+
+if __name__ == '__main__':
+    rospy.init_node('nav_Server')
+    ax=np.array([10,10,10,5,1])
+    ay=np.array([5,3.5,2,2,2]) 
+    bot1 = Bot(str(1))   # bot id
+    bot1.cx = (ax-1)*0.15
+    bot1.cy = (ay-1)*0.15
+    bot1.target_indx = 0
+    _pub_cntl = rospy.Publisher('/commu', com_msg,queue_size=1)
+    feed_sub = rospy.Subscriber('/feedback',localizemsg,feed_cb,bot1)
+    bot1.control(_pub_cntl)
+    rospy.spin()
